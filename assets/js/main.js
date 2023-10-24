ae5d66846ef4fd11350daf561321062aa4841800
danieltky <72296991+danieltky@users.noreply.github.com>
2023-10-25 01:43:10 +0800

    added email validation and replaced page animation
    - removed swup animation css classes, html tags, and its cdn.
    - added animate.css animation to style effect on page load.
    - added js validation for contact form's email input field.
    - set contact form's input fields' values to be cleared when user redirects backwards from thank you page.
    - moved inline script for getting the current year from each page, to main.js  and placed it in a onbeforeload function.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,36 @@
 
-const swup = new Swup();
+// Refreshes form input fields whenever user clicks on the back button on the thank you page
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
 
-document.addEventListener('swup:contentReplaced', function () {
-    // Force a page refresh
-    window.location.reload();
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('year').textContent = new Date().getFullYear();
 });
+
+// Validate email input
+function validateEmailField(email, emailInputField, submitBtn) {
+	var validRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+	if (!validRegex.test(email)) {
+		if (emailInputField.classList.contains("is-valid")) {
+			emailInputField.classList.remove("is-valid");
+		}
+
+		emailInputField.focus();
+		submitBtn.disabled = true;
+		emailInputField.classList.add("is-invalid");
+		return false;
+	} else {
+		if (emailInputField.classList.contains("is-invalid")) {
+			emailInputField.classList.remove("is-invalid");
+		}
+
+		emailInputField.focus();
+		submitBtn.disabled = false;
+		emailInputField.classList.add("is-valid");
+		return true;
+	}
+}
